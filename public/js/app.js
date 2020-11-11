4518b5a04024c3bdc06a23e5a0153ec41f9b20d3
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2020-11-11 20:44:49 +0530

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,58 @@
-console.log('client side javascript file is loaded')
+console.log('client side javascript file is loaded');
+
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) =>{
+//     response.json().then((data) => {
+//         console.log(data);
+//     })
+// })
+
+// fetch('http://localhost:3008/weather?address=suratkal').then((response) => {
+//     response.json().then((data) => {
+//         if (data.error) {
+//             console.log(data.error);
+//         } else {
+//             console.log(data.location);
+//             console.log(data.forecast);
+//         }
+//     });
+// });
+
+
+const weatherForm = document.querySelector('form');
+const searchValue = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+
+//messageOne.textContent = 'From Java Script';
+
+weatherForm.addEventListener('submit',(e) => {
+
+    e.preventDefault();
+
+    const locationValue = searchValue.value;
+
+    //console.log('testing...!');
+   // console.log(locationValue);
+
+   messageOne.textContent = 'Loading....';
+    messageTwo.textContent= '';
+
+fetch('http://localhost:3008/weather?address='+ locationValue).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            //console.log(data.error);
+            messageOne.textContent = data.error;
+        } else {
+            // console.log(data.location);
+            // console.log(data.forecast);
+
+            
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forecast;
+        }
+    });
+});
+
+});
