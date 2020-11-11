21fa9fa9e7827f5fdda45f41c8aff57ff17df7e1
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2020-11-11 20:46:03 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,9 @@
 console.log('client side javascript file is loaded');
 
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) =>{
-//     response.json().then((data) => {
-//         console.log(data);
-//     })
-// })
-
-// fetch('http://localhost:3008/weather?address=suratkal').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error);
-//         } else {
-//             console.log(data.location);
-//             console.log(data.forecast);
-//         }
-//     });
-// });
-
-
 const weatherForm = document.querySelector('form');
 const searchValue = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
-
 
 //messageOne.textContent = 'From Java Script';
 
@@ -47,7 +27,6 @@ fetch('http://localhost:3008/weather?address='+ locationValue).then((response) =
         } else {
             // console.log(data.location);
             // console.log(data.forecast);
-
             
             messageOne.textContent = data.location;
             messageTwo.textContent = data.forecast;
